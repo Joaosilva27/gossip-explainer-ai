823d32e297952cc6153b5efdf625d20845f8535f
Joaosilva27 <joaosilva7875@gmail.com>
2025-04-26 11:51:32 +0200

    gemini demo setup
    * npm install @google/genai
    
    * added api key to .env file for privacy
    
    * added default gemini function from official google docs - working with hard-coded prompt

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,17 @@
 import "./App.css";
+import { GoogleGenAI } from "@google/genai";
+
+const ai = new GoogleGenAI({ apiKey: import.meta.env.VITE_GEMINI_API_KEY });
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: "gemini-2.0-flash",
+    contents: "whats 1*1",
+  });
+  console.log(response.text);
+}
+
+main();
 
 function App() {
   return (
