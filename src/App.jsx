59a20678f3e7ba78e5d11c8087be70649cd32100
Joaosilva27 <joaosilva7875@gmail.com>
2025-04-26 14:35:05 +0200

    working hard-coded gossip demo
    * moved prompt fed to AI to its own file
    
    * new aiResponse and userPrompt state (for now userPrompt is hard-coded)
    
    * added onGossipGeneration function and a respective button with onClick property

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,28 @@
 import "./App.css";
 import { GoogleGenAI } from "@google/genai";
+import { geminiPrompt } from "./Prompt";
+import { useState } from "react";
 
 const ai = new GoogleGenAI({ apiKey: import.meta.env.VITE_GEMINI_API_KEY });
 
-async function main() {
-  const response = await ai.models.generateContent({
-    model: "gemini-2.0-flash",
-    contents: "whats 1*1",
-  });
-  console.log(response.text);
-}
-
-main();
-
 function App() {
+  const [aiResponse, setAiResponse] = useState(undefined);
+  const [userPrompt, setUserPrompt] = useState("World war 2");
+
+  async function onGossipGeneration() {
+    const response = await ai.models.generateContent({
+      model: "gemini-2.0-flash",
+      contents: geminiPrompt + userPrompt,
+    });
+    console.log(response.text);
+    setAiResponse(response.text);
+  }
+
   return (
-    <div>
-      <h1 className='text-2xl'>sassy explainer</h1>
+    <div className='flex flex-col'>
+      <h1 className='text-2xl'>Gossip AI</h1>
+      <button onClick={onGossipGeneration}>Generate your Gossip, queen</button>
+      {aiResponse}
     </div>
   );
 }
---
+++ src/Prompt.tsx
@@ -0,0 +1,6 @@
+export const geminiPrompt = `You are a gossip AI explainer, you will be provided with a input and you must explain it as a gossip.
+
+Although you are explaining it as a gossip, it must still be informing. Your mission is to gossip while still explaining a lot of information, 
+to go into detail, and in general to explain so well that anybody would understand. Because after all, your purpose is to teach and for the user to LEARN.
+This is the input: 
+`;
